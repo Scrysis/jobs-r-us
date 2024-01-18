88dffd1dc150daf4b04494023a7669d0b77e8a18
jessclark1234 <jessicaclark7890@gmail.com>
2024-01-17 21:06:34 -0800

    added starting code for login

---
+++ login.js
@@ -0,0 +1,24 @@
+const loginForm = async (event) => {
+    event.preventDefault();
+
+    const username = document.querySelector('#user-login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
+
+    if (username && password) {
+        const response = await fetch('/api/users/login', {
+            method: 'POST',
+            body: JSON.stringify({ username, password }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (response.ok) {
+            document.location.replace('/');
+        } else {
+            alert('Could not login to account. Please try again');
+        }
+    }
+};
+
+document
+    .querySelector('.login')
+    .addEventListener('submit', loginForm);
