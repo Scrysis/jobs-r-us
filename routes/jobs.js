05a5aa96e4683bcb38fb5249254671cffcf4c88c
Scrysis <Scrysis@users.noreply.github.com>
2024-01-22 22:27:54 -0700

    Added rough sketched comments for match-sorter implementation

--- routes/jobs.js
+++ routes/jobs.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Sequelize = require('sequelize');
+// const {matchSorter} = require('match-sorter');   // import for the match-sorter package
 
 const sequelize = new Sequelize('job', 'username', 'password', {
   host: 'localhost',
@@ -32,3 +33,17 @@ router.get('/jobs/:id', async (req, res) => {
     res.status(500).json({ message: 'Error retrieving job' });
   }
 });
+
+/* Rough sketch of match-sorter implementation. https://www.npmjs.com/package/match-sorter?activeTab=readme#usage
+So you can take search terms retrieved from the form and pass them into the route.  Use the term to plug
+into the match-sorter function.  Super rough, hasty sketched version, so it will need tweaking. */
+/* router.get('/jobs/:${search_term}, async (req, res) => {
+  try {
+    const jobs = await sequelize.model('Job').findAll();
+    const results = matchSorter(jobs, search_term);
+    res.status(200).json(results);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({message: 'Error'});
+  }
+}) */
