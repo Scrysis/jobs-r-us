aebc7c7b6d209882610d84b3c77122b8d8b8977c
Scrysis <Scrysis@users.noreply.github.com>
2024-01-22 20:03:00 -0700

    modifications to user model

--- models/User.js
+++ models/User.js
@@ -1,5 +1,5 @@
 const { Model, DataTypes } = require("sequelize");
-const sequelize = require('../config/connection');
+const sequelize = require("../config/connection");
 
 class User extends Model {}
 
@@ -27,8 +27,28 @@ User.init(
         isEmail: true,
       },
     },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [8],
+      },
+    },
   },
   {
+    hooks: {
+      beforeCreate: async (newUserData) => {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+      beforeUpdate: async (updatedUserData) => {
+        updatedUserData.password = await bcrypt.hash(
+          updatedUserData.password,
+          10
+        );
+        return updatedUserData;
+      },
+    },
     sequelize,
     timestamps: false,
     freezeTableName: true,
