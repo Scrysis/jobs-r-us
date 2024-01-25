d7aa85f161d286b186aacee9d5b9ca42e26c72db
Scrysis <Scrysis@users.noreply.github.com>
2024-01-24 22:04:20 -0700

    fixed server.js

--- server.js
+++ server.js
@@ -36,10 +36,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
-const hbs = exphbs.create({ helpers });
 
-app.engine("handlebars", hbs.engine);
-app.set("view engine", "handlebars");
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
