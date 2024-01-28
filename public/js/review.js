93efb1e440042030e06f1a1a34b5e8c3fcf82527
jessclark1234 <jessicaclark7890@gmail.com>
2024-01-28 11:23:22 -0800

    saving changes

---
+++ public/js/review.js
@@ -0,0 +1,19 @@
+var createRating = () => {
+    const radio = document.querySelector('.radio');
+    radio.addEventListener('click', () => {
+    });
+    
+};
+
+
+// // // Function to create a radio element and append to parent element 
+// function createRadio(name, parentElem, checked = false) { 
+//     const elem = document.createElement('input'); 
+//     elem.type = 'radio'; 
+//     elem.name = name; 
+//     elem.checked = checked; 
+//     parentElem.appendChild(elem); 
+//   } 
+   
+//   // Example call that adds new radio to the body 
+//   createRadio('test', document.body, true);
